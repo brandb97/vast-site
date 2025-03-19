b26ca7e481c3349fae73b36be62a419e1765be70
lidongyan <502024330056@smail.nju.edu.cn>
2025-03-19 16:16:59 +0800

    add basic introduction html

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import mlirLogo from './assets/mlir.svg'
+import tobLogo from './assets/tob.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vast Development Report</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://mlir.llvm.org" target="_blank">
+                    <img src={mlirLogo} className="logo" alt="MLIR logo"/>
+                </a>
+                <a href="https://trailofbits.github.io/vast/" target="_blank">
+                    <img src={tobLogo} className="logo white" alt="TrailOfBits logo"/>
+                </a>
+            </div>
+            <h1>Vast: Cover every analysis you need</h1>
+            <section className="section" id="news">
+                <h2>Latest News</h2>
+                <div className="box">
+                    <p>🎉<b>(03/2025)</b> vast拥有了第一个网站</p>
+                </div>
+            </section>
+            <section className="section" id="features">
+                <h2>Features</h2>
+                <a href="features/feature.html" target="_blank">
+                    <button>view all features</button>
+                </a>
+            </section>
+            <section className="section" id="compile-result">
+                <h2>IR Generation on Real Project</h2>
+                <a href="compiler-result/compiler.html" target="_blank">
+                    <button>view all c/c++ projects vast can compile</button>
+                </a>
+            </section>
+            <section className="section" id="analysis-result">
+                <h2>Static Analysis Bug Report</h2>
+                <a href="analysis-result/analysis.html" target="_blank">
+                    <button>view all bug report</button>
+                </a>
+            </section>
+        </>
+    )
 }
 
 export default App
